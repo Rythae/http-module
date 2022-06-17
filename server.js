759563bf647ac5d1d3a54a8fa5fd0f990d882612
rythae <auxano42@gmail.com>
2022-06-17 20:45:37 +0100

    added routes

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<h1>Hello, World</h1?");
+  res.setHeader("Content-Type", "text/html");
+
+  if (req.url === "/about") {
+    res.write(
+      "<h1>Welcome to About Page!</h1>" +
+        "<p>" +
+        "Hi there! I'm Ree, a Lagos-based frontend developer, and an aspiring" +
+        "full-stack developer. Currently started my backend development journey" +
+        "as an intern at Zuri" +
+        "</p>"
+    );
+    res.end();
+  } else if (req.url === "/contact") {
+    res.write(
+      "<h1>Welcome to Contact Page!</h1>" +
+        "<h3>You can reach me on social media via:</h3>" +
+        "<p>" +
+        "Github: <a>https://github.com/Rythae</a>" +
+        "</p>" +
+        "<p>" +
+        "LinkedIn: <a>https://www.linkedin.com/in/rita-emili-4a9a97165/</a>" +
+        "</p>"
+    );
+    res.end();
+  } else if (req.url === "/") {
+    res.write(
+      "<h1>Welcome to Home Page!</h1> " +
+        "<p>Full Name: Rita Emili</p>" +
+        "<p>Job title: Backend developer</p>"
+    );
+    res.end();
+  } else {
+    // do a 302 redirect
+    res.writeHead(302, {
+      location: "/",
+    });
+    res.end();
   }
 });
 server.listen(5000);
-console.log("Server is listening at port 9000");
+console.log("Server is listening at port 5000");
